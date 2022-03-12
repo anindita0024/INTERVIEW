f949a9b5b2f798f83f3a5f8785407bc76bb858ae
anindita <aninditapanda277@gmail.com>
2022-03-12 22:32:28 +0530

    problem_solving_12Maech

--- problem_solving_2March/9.palindrome-number.js
+++ problem_solving_2March/9.palindrome-number.js
@@ -9,8 +9,14 @@
  * @param {number} x
  * @return {boolean}
  */
-var isPalindrome = function(x) {
-    
+ const isPalindrome = (x) => {
+    let str = x.toString();
+    for(let i = 0; i< str.length/2; i++){
+        if (str[i] !== str[str.length -1-i]){
+            return false;
+        }
+    }
+    return true
 };
 // @lc code=end
 
